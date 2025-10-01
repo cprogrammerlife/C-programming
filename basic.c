1984c1d60ea904788939ecdad6f7f58c19037c0d
cprogrammerlife <backup.758.io@gmail.com>
2025-10-01 23:26:43 +0530

    hello world in c

--- basic.c
+++ basic.c
@@ -1,2 +1,7 @@
 # C-programming
 This is a project where all the basic programs of c language are accessible to everyone for reference.
+#include<stdio.h>
+int main(){
+  printf("hello world");
+return 0;
+}
